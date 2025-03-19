0cb0403a7f044467af5b0c7e4db4400016e47709
DawnBreaker <tunganhngo207@gmail.com>
2025-03-19 21:18:56 +0700

    feat: update config

--- BE/src/main/java/com/project/blog/infra/config/ModelMapperConfig.java
+++ BE/src/main/java/com/project/blog/infra/config/ModelMapperConfig.java
@@ -3,14 +3,28 @@ package com.project.blog.infra.config;
 import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import com.project.blog.api.model.user.UserUpdate;
+import com.project.blog.domain.model.User;
+
+@Configuration
 public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
 	var modelMapper = new ModelMapper();
 	modelMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
-   
-	var updateUserTypeMap = modelMapper.createTypeMap(UserUPdate, null)
+
+	var updateUserTypeMap = modelMapper.createTypeMap(UserUpdate.class, User.class);
+	updateUserTypeMap.<String>addMapping(UserUpdate::getBio,
+		(destination, value) -> destination.getProfile().setBio(value));
+	updateUserTypeMap.<String>addMapping(UserUpdate::getImage,
+		(destination, value) -> destination.getProfile().setImage(value));
+	updateUserTypeMap.<String>addMapping(UserUpdate::getUsername,
+		(destination, value) -> destination.getProfile().setUsername(value));
+
+	return modelMapper;
+
     }
 }
