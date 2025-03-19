9ccae606b7ec4a8cc7e2c93ea5a6d2a6e0fe5aa7
DawnBreaker <tunganhngo207@gmail.com>
2025-03-19 18:15:12 +0700

    feat: update api model user

--- BE/src/main/java/com/project/blog/api/model/user/UserResponse.java
+++ BE/src/main/java/com/project/blog/api/model/user/UserResponse.java
@@ -3,11 +3,13 @@ package com.project.blog.api.model.user;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.project.blog.api.model.BaseResponse;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @JsonTypeName("user")
 public class UserResponse extends BaseResponse {
 
